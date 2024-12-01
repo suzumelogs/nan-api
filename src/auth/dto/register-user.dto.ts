563daaf636fae33ded43c6e52d42a864d143e58e
cuongthieu <suzume.logs@gmail.com>
2024-12-01 09:12:09 +0700

    Update

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -11,7 +11,6 @@ import {
 export class RegisterUserDto {
   @ApiProperty({
     description: 'Tên người dùng',
-    example: 'John Sample',
   })
   @IsString()
   @MinLength(3)
@@ -20,29 +19,18 @@ export class RegisterUserDto {
   @ApiProperty({
     description: 'Email người dùng',
     uniqueItems: true,
-    example: 'youremail@example.com',
   })
   @IsEmail()
   email: string;
 
   @ApiProperty({
-    description:
-      'Mật khẩu: Tối thiểu 6 ký tự, 1 chữ cái viết hoa, 1 chữ cái viết thường và 1 số',
-    example: 'Password123',
+    description: 'Mật khẩu',
   })
   @IsString()
-  @MinLength(6)
-  @MaxLength(16)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
-    message:
-      'Mật khẩu phải chứa ít nhất một chữ cái viết hoa, một chữ cái viết thường và một số',
-  })
-  @NotContains(' ', { message: 'Mật khẩu không được chứa khoảng trắng' })
   password: string;
 
   @ApiProperty({
     description: 'Xác nhận mật khẩu, phải giống với mật khẩu',
-    example: 'Password123',
   })
   @IsString()
   passwordconf: string;
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -6,8 +6,6 @@ import { RegisterUserDto } from 'src/auth/dto/register-user.dto';
 export class CreateUserDto extends RegisterUserDto {
   @ApiProperty({
     description: 'Vai trò người dùng (admin, user)',
-    default: 'user',
-    example: 'user',
   })
   @IsString()
   @IsOptional()
